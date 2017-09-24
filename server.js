78ee8c3a71bcd12ea038fe6650fadad28d5c09ef
Vaibhav Kulshrestha <vaibhav1kulshrestha@gmail.com>
2017-09-24 14:14:36 +0530

    Basic GET and POST handling

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
 
-app.use(express.static('public'));
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
 	res.send('Hello, world!');
 });
 
-app.listen(3000,  function() {
-	console.log('Example app listening on port 3000.');
+app.get('/index.html', function(req, res) {
+   res.sendFile( __dirname + "/" + "index.html");
+})
+
+app.get('/process_get', function(req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+});
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+});
+
+var server = app.listen(3000,  function() {
+	var host = server.address().address;
+   var port = server.address().port;
+   console.log("Example app listening at http://%s:%s", host, port);
 });
